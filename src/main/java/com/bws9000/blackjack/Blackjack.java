84e94b4d42bc76ddf302c20b9e1fb1b6dcbe5725
bws9000 <armorsoft@gmail.com>
2019-02-21 12:54:33 -0500

    first commit

---
+++ src/main/java/com/bws9000/blackjack/Blackjack.java
@@ -0,0 +1,8 @@
+package com.bws9000.blackjack;
+
+public class Blackjack {
+    public static void main(String argv[]){
+        System.out.println(Config.getLocalSocketURI());
+        System.out.println(Config.getRemoteSocketURI());
+    }
+}
---
+++ src/main/java/com/bws9000/blackjack/Config.java
@@ -0,0 +1,33 @@
+package com.bws9000.blackjack;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public final class Config {
+
+    private static Properties properties;
+
+    private static Properties getProperties() {
+        if(properties == null) {
+            try {
+                properties = new Properties();
+                FileInputStream in = new FileInputStream("config.properties");
+                properties.load(in);
+                in.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }else{
+            //
+        }
+        return properties;
+    }
+
+    public static String getRemoteSocketURI(){
+        return getProperties().getProperty("REMOTE_DEV_URI");
+    }
+
+    public static String getLocalSocketURI(){
+        return getProperties().getProperty("LOCAL_DEV_URI");
+    }
+}
